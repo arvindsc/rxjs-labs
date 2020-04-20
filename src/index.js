99676ae8207af64b28b34e8fd9885292ad47ffc3
Arvind Chauhan <arvind.chauhan@macquarie.com>
2020-04-20 11:03:18 +1000

    multicast observables

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
-import { of } from "rxjs";
+import { Subject, interval } from "rxjs";
+import { tap, multicast } from "rxjs/operators";
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
-of("Hello", "RxJS").subscribe(console.log);
+const observer = {
+  next: (value) => console.log("next:", value), //?
+  error: (error) => console.log("error:", error),
+  complete: () => console.log("complete"),
+};
+
+const interval$ = interval(1000).pipe(
+  tap((i) => console.log(`Interval  ${i}`))
+);
+const multicastedInterval$ = interval$.pipe(multicast(() => new Subject()));
+
+multicastedInterval$.connect();
+
+const subOne = multicastedInterval$.subscribe(observer);
+const subTwo = multicastedInterval$.subscribe(observer);
+
+setTimeout(() => {
+  subOne.unsubscribe();
+  subTwo.unsubscribe();
+}, 3000);
+setTimeout(() => {
+  const subThree = multicastedInterval$.subscribe(observer);
+}, 4000);
